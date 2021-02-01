7b966ea0cd7df62f741556bde7443b343fdc90ea
George <leonardogeorge95@gmail.com>
2021-02-01 15:05:07 +0200

    Update Predicate

--- src/lambdas/PredicateDemo.java
+++ src/lambdas/PredicateDemo.java
@@ -1,5 +1,5 @@
 /*
-The approach using anonymous classes.
+The approach using lambda.
  */
 package lambdas;
 
@@ -9,12 +9,7 @@ public class PredicateDemo {
 
 
     public static void main(String[] args) {
-        Predicate<Integer> predicate = new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer integer) {
-                return integer % 2 == 0;
-            }
-        };
+        Predicate<Integer> predicate = (t) -> t % 2 == 0;
         System.out.println(predicate.test(10));
 
     }
