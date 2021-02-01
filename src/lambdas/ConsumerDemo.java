179a5d2e83732f4b954075c7eb7d31829ab27df3
George <leonardogeorge95@gmail.com>
2021-02-01 14:55:18 +0200

    Update Consumer

--- src/lambdas/ConsumerDemo.java
+++ src/lambdas/ConsumerDemo.java
@@ -1,18 +1,20 @@
 /*
-The traditional approach without using anonymous classes or lambdas.
+This approach will use anonymous classes.
  */
 package lambdas;
 
 import java.util.function.Consumer;
 
-public class ConsumerDemo implements Consumer<Integer> {
-    @Override
-    public void accept(Integer i) {
-        System.out.println("Printing: " + i);
-    }
+public class ConsumerDemo {
+
 
     public static void main(String[] args) {
-        ConsumerDemo consumerDemo = new ConsumerDemo();
-        consumerDemo.accept(10);
+        Consumer consumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer i) {
+                System.out.println("Printing: " + i);
+            }
+        };
+        consumer.accept(10);
     }
 }
