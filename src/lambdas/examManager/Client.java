7c7339f5f40ef6f5934d5b01b205efe2044b9875
George <leonardogeorge95@gmail.com>
2021-02-03 16:14:58 +0200

    Update client

--- src/lambdas/examManager/Client.java
+++ src/lambdas/examManager/Client.java
@@ -1,6 +1,6 @@
 /*
-We achieved a structure that permits us to pass functionality to other class.
-However, this is not an elegant solution, because we need to create all the classes and wrap the functionality.
+A simpler solution to this problem will be to use lambdas.
+We can get rid of AverageScoreCalculatorImpl, HighestScoreCalculatorImpl
  */
 package lambdas.examManager;
 
@@ -8,12 +8,28 @@ import lambdas.calculators.AverageScoreCalculatorImpl;
 import lambdas.calculators.CustomCalculationInterface;
 import lambdas.calculators.HighestScoreCalculatorImpl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
+        CustomCalculationInterface lambdaAverage = scores -> {
+            Double average = 0d;
+            for(Double nextScore : scores){
+                average += nextScore;
+            }
+            return average / scores.size();
+        };
+        CustomCalculationInterface lambdaHighest = scores -> {
+            Double highest = 0d;
+            for(Double nextScore : scores){
+                highest = Math.max(highest, nextScore);
+            }
+            return highest;
+        };
         ExamManager examManager = new ExamManager();
-        System.out.println("Average score: " + examManager.customCalculation(new AverageScoreCalculatorImpl()));
-        System.out.println("Highest score: " + examManager.customCalculation(new HighestScoreCalculatorImpl()));
+        System.out.println("Average score: " + examManager.customCalculation(lambdaAverage));
+        System.out.println("Highest score: " + examManager.customCalculation(lambdaHighest));
     }
 }
