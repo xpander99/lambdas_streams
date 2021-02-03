bff8a308b19b6ec6bf91ca0c55bd407fb8f0169f
George <leonardogeorge95@gmail.com>
2021-02-03 17:14:00 +0200

    Update Client

--- src/lambdas/examManager/Client.java
+++ src/lambdas/examManager/Client.java
@@ -1,7 +1,8 @@
 /*
 A simpler solution to this problem will be to use lambdas.
 We can get rid of CustomCalculationInterface too, because we can use a predefined
-functional interface in java, namely Function.
+functional interface in java, namely Function which takes an input of type T and returns and output
+of type R.
  */
 package lambdas.examManager;
 
@@ -27,5 +28,6 @@ public class Client {
         ExamManager examManager = new ExamManager();
         System.out.println("Average score: " + examManager.customCalculation(lambdaAverage));
         System.out.println("Highest score: " + examManager.customCalculation(lambdaHighest));
+        System.out.println("The first score: " + examManager.customCalculation((scores) -> scores.get(0)));
     }
 }
