0c77e25b52f32344b6e388ed08f43a73e760179d
George <leonardogeorge95@gmail.com>
2021-02-03 17:52:11 +0200

    Refactor

--- src/lambdas/book/Client.java
+++ src/lambdas/book/Client.java
@@ -3,11 +3,11 @@ package lambdas.book;
 import java.util.List;
 
 public class Client {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BookCollection bookCollection = new BookCollection();
         List<Book> foundBooks = bookCollection.findBooks(book -> book.getTitle() == "Ion");
 
-        for(Book book : foundBooks){
+        for (Book book : foundBooks) {
             System.out.println(book.getAuthor() + " " + book.getTitle());
         }
     }
